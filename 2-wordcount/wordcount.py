2bbbf34ba833535f04ad321a884b5159bb3f3ddb
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-08-10 01:42:21 +0530

    Word frequency analysis

---
+++ 2-wordcount/wordcount.py
@@ -0,0 +1,17 @@
+excerpt = """
+The Babel fish, said The Hitchhiker’s Guide to the Galaxy quietly,
+is small, yellow, and leech-like, and probably the oddest thing in
+the Universe. It feeds on brainwave energy received not from its own
+carrier but from those around it. It absorbs all unconscious mental
+frequencies from this brainwave energy to nourish itself with. It then
+excretes into the mind of its carrier a telepathic matrix formed by
+combining the conscious thought frequencies with nerve signals picked
+up from the speech centres of the brain which has supplied them. The
+practical upshot of all this is that if you stick a Babel fish in your
+ear you can instantly understand anything said to you in any form of
+language. The speech patterns you actually hear decode the brainwave
+matrix which has been fed into your mind by your Babel fish.
+"""
+
+# Write a python program to print the three most common words from the
+# text above
