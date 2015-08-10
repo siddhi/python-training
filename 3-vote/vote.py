8303cae5b5bc5423e4f30e10809ca11870240ff1
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2015-08-10 21:13:39 +0530

    Updated vote solution

--- 3-vote/vote.py
+++ 3-vote/vote.py
@@ -1,20 +1,22 @@
 votes = {}
 voters = []
 
-for line in open("votes.txt"):
-    person, vote = [item.strip() for item in line.split("-")]
-    if person.lower() in voters:
-        continue
 
-    if vote in votes:
-        votes[vote] += 1
-    else:
-        votes[vote] = 1
-    voters.append(person.lower())
+with open("votes.txt") as file_obj:
+    for line in file_obj:
+        person, vote = [item.strip() for item in line.split("-")]
+        if person.lower() in voters:
+            continue
 
-winner = (None, 0)
-for colour, votes in votes.items():
-    if votes > winner[1]:
-        winner = (colour, votes)
+        if vote in votes:
+            votes[vote] += 1
+        else:
+            votes[vote] = 1
+        voters.append(person.lower())
 
-print("Annnd, the winner is {0} with {1} votes!".format(winner[0], winner[1]))
+winner = ("", 0)
+for colour in votes:
+    if votes[colour] > winner[1]:
+        winner = (colour, votes[colour])
+
+print("Annnd, the winner is {0[0]} with {0[1]} votes!".format(winner))
