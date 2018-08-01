19628da62b9ae49fa13b00f254cfd42fe8d0b79e
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2018-08-02 00:06:47 +0530

    Replaced hangman implementation with a simpler version

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,30 +1,34 @@
 import random
 
-f = open("words_alpha.txt")
-words = []
-for line in f:
-    words.append(line.strip())
-word = random.choice(words).lower()
+with open("words.txt") as fp:
+    words = fp.read().split("\n")
 
-guessed_letters = ["-"] * len(word)
-has_won = False
 num_guesses = 8
-while not has_won and num_guesses > 0:
-    print("".join(guessed_letters))
-    print("You have {} guesses remaining".format(num_guesses))
-    guess = input("Enter a letter to guess: ").lower()
-    correct_guess = False
-    for index, letter in enumerate(word):
-        if letter == guess:
-            guessed_letters[index] = guess
-            correct_guess = True
-    if "-" not in guessed_letters:
-        has_won = True
-    if not correct_guess:
-        num_guesses -= 1
+correct_letters = set()
+wrong_letters = set()
+choice = random.choice(words)
 
-if has_won:
-    print("You won! The word was {}".format(word))
-else:
-    print("You lost. The word was {}".format(word))
+def user_wins():
+    return set(choice) == correct_letters
 
+def guesses_over():
+    return num_guesses == len(wrong_letters)
+
+def game_over():
+    return user_wins() or guesses_over()
+
+def get_char_to_display(char):
+    if char in correct_letters:
+        return char
+    return "-"
+
+while not game_over():
+    print("You have {} wrong guesses remaining".format(num_guesses-len(wrong_letters)))
+    print([get_char_to_display(char) for char in choice])
+    user_char = input("Enter a character: ")
+    if user_char in choice:
+        correct_letters.add(user_char)
+    else:
+        wrong_letters.add(user_char)
+
+print("The word is {}".format(choice))
